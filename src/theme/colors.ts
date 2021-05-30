57b92eda87cbba93a05eca2244c8403e854155ca
Samuel <awesomeness891@gmail.com>
2021-05-30 20:54:29 +0800

    w

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -17,7 +17,7 @@ export const brandColors = {
 export const lightColors: Colors = {
   ...baseColors,
   ...brandColors,
-  background: "#FFFFFF",
+  background: "#FFF8DC",
   backgroundDisabled: "#E9EAEB",
   contrast: "#191326",
   invertedContrast: "#FFFFFF",
