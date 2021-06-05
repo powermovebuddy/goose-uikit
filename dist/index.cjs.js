726957314665564a4a58d065000f5f7c5b7daf71
NeptuneKing <samuelchngweiming@hotmail.com>
2021-06-06 00:51:55 +0800

    w

--- dist/index.cjs.js
+++ dist/index.cjs.js
@@ -2146,7 +2146,7 @@ var socials = [
     {
         label: "Twitter",
         icon: "TwitterIcon",
-        href: "https://twitter.com/WolvesDenDefi/",
+        href: "https://twitter.com/wolvesdendefi/",
     },
 ];
 var MENU_HEIGHT = 64;
@@ -2740,7 +2740,7 @@ var brandColors = {
 var lightColors = __assign(__assign(__assign({}, baseColors), brandColors), { background: "#15223b", backgroundDisabled: "#E9EAEB", contrast: "#191326", invertedContrast: "#FFFFFF", input: "#eeeaf4", tertiary: "#EFF4F5", text: "#c0c0c0", textDisabled: "#BDC2C4", textSubtle: "#c0c0c0", borderColor: "#C0C0C0", card: "#434E61", gradients: {
         bubblegum: "linear-gradient(139.73deg, #E6FDFF 0%, #F3EFFF 100%)",
     } });
-var darkColors = __assign(__assign(__assign({}, baseColors), brandColors), { secondary: "#708090", background: "#343135", backgroundDisabled: "#3c3742", contrast: "#FFFFFF", invertedContrast: "#191326", input: "#483f5a", primaryDark: "#708090", tertiary: "#353547", text: "#add8e6", textDisabled: "#666171", textSubtle: "#add8e6", borderColor: "#524B63", card: "#27262c", gradients: {
+var darkColors = __assign(__assign(__assign({}, baseColors), brandColors), { secondary: "#c0c0c0", background: "#15223b", backgroundDisabled: "#E9EAEB", contrast: "#191326", invertedContrast: "#FFFFFF", input: "#eeeaf4", primaryDark: "#708090", tertiary: "#EFF4F5", text: "#c0c0c0", textDisabled: "#BDC2C4", textSubtle: "#c0c0c0", borderColor: "#c0c0c0", card: "#434E61", gradients: {
         bubblegum: "linear-gradient(139.73deg, #313D5C 0%, #3D2A54 100%)",
     } });
 
